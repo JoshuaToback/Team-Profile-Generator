01158cce607ac6135011136e5c1c82ddde39620a
JoshuaToback <joshuatoback@gmail.com>
2022-11-19 15:56:42 -0500

    Added tests and lib files, need to split index to generateHTML.js

---
+++ index.js
@@ -0,0 +1,206 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const generateCards = require("./src/page-template");
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const distPath = path.join(DIST_DIR, 'team.html');
+
+const render = require('./src/page-template.js');
+
+const teamMembers = [];
+const idArray = [];
+
+// Inform user of usage
+console.log(
+  '\nWelcome to the team generator!\nUse `npm run reset` to reset the dist/ folder\n'
+);
+
+function appMenu() {
+  function createManager() {
+    console.log('Please build your team 👥');
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'managerName',
+          message: "What is the team manager's name?",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Please enter at least one character.';
+          },
+        },
+        {
+          type: 'input',
+          name: 'managerId',
+          message: "What is the team manager's id?",
+          validate: (answer) => {
+            const pass = answer.match(/^[1-9]\d*$/);
+            if (pass) {
+              return true;
+            }
+            return 'Please enter a positive number greater than zero.';
+          },
+        },
+        {
+          type: 'input',
+          name: 'managerEmail',
+          message: "What is the team manager's email?",
+          validate: (answer) => {
+            const pass = answer.match(/\S+@\S+\.\S+/);
+            if (pass) {
+              return true;
+            }
+            return 'Please enter a valid email address.';
+          },
+        },
+        {
+          type: 'input',
+          name: 'managerOfficeNumber',
+          message: "What is the team manager's office number?",
+          validate: (answer) => {
+            const pass = answer.match(/^[1-9]\d*$/);
+            if (pass) {
+              return true;
+            }
+            return 'Please enter a positive number greater than zero.';
+          },
+        },
+      ])
+      .then((answers) => {
+        const manager = new Manager(
+          answers.managerName,
+          answers.managerId,
+          answers.managerEmail,
+          answers.managerOfficeNumber
+        );
+        teamMembers.push(manager);
+        idArray.push(answers.managerId);
+        createTeam();
+      });
+  }
+
+  function createTeam() {
+    return inquirer
+		.prompt([
+			{
+				type: "list",
+				name: "teamMember",
+				message: "Would you like to add an engineer or intern?",
+				choices: [
+					"Engineer",
+					"Intern",
+					"Done"
+				]
+			}
+    ])
+		.then(answer => {
+			switch (answer.teamMember) {
+				case "Engineer":
+					addEngineer();
+					break;
+				case "Intern":
+					addIntern();
+					break;
+				default:
+					generateCards(teamMembers, idArray);
+			}
+		});
+  }
+
+  function addEngineer() {
+    return inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "engineerName",
+				message: "What is the engineer's name?"
+			},
+			{
+				type: "input",
+				name: "engineerId",
+				message: "what is the engineer's employee number?"
+			},
+			{
+				type: "input",
+				name: "engineerEmail",
+				message: "what is the engineer's email address?"
+			},
+			{
+				type: "input",
+				name: "engineerGithub",
+				message: "what is the engineer's git hub username?"
+			}
+		])
+		.then(function({
+			engineerName,
+			engineerId,
+			engineerEmail,
+			engineerGithub
+		}) {
+			const engineer = new Engineer(
+				engineerName,
+				engineerId,
+				engineerEmail,
+				engineerGithub
+			);
+			teamMembers.push(engineer);
+      idArray.push(answers.engineerId);
+			createTeam();
+		});
+  }
+
+  function addIntern() {
+    return inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "internName",
+				message: "What is the intern's name?"
+			},
+			{
+				type: "input",
+				name: "internId",
+				message: "what is the intern's employee number?"
+			},
+			{
+				type: "input",
+				name: "internEmail",
+				message: "what is the intern's email address?"
+			},
+			{
+				type: "input",
+				name: "internSchool",
+				message: "what is the name of the school the intern is attending?"
+			}
+		])
+		.then(function({ internName, internId, internEmail, internSchool }) {
+			const intern = new Intern(
+				internName,
+				internId,
+				internEmail,
+				internSchool
+			);
+			teamMembers.push(intern);
+      idArray.push(answers.internId);
+			createTeam();
+		});
+  }
+
+  function buildTeam() {
+    // Create the output directory if the dist path doesn't exist
+    if (!fs.existsSync(DIST_DIR)) {
+      fs.mkdirSync(DIST_DIR);
+    }
+    fs.writeFileSync(distPath, render(teamMembers), 'utf-8');
+  }
+
+  createManager();
+}
+
+appMenu();
